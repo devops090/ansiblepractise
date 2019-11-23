5b950ff61f8f5714dd9608c2a7ce0bf3232e238d
devops090 <51469018+devops090@users.noreply.github.com>
2019-11-24 02:42:15 +0530

    Update runtime.sh
    chghgv

--- runtime.sh
+++ runtime.sh
@@ -3,4 +3,8 @@ IDNO=$(echo $VER | awk -F - '{print $1F}')
 FINALVER=$(curl http://admin:admin123@10.128.0.7:8081/service/rest/repository/browse/studenapp-snapshot/com/jdevs/studentapp/$VER/ | html2text | grep "$IDNO-" | tail -1)
 http_url=http://admin:admin123@10.128.0.7:8081/repository/studenapp-snapshot/com/jdevs/studentapp/
 STUDENTWAR=$http_url/$VER/studentapp-$FINALVER.war)
+echo $VER
+echo $IDNO
+echo $FINALVER
 echo $STUDENTWAR
+ansible-playbook playbooks/studentapp-deploy.yaml -i inventory -u student --extra-vars "ansible_sudo_pass=student" "STUDENTWAR=$STUDENTWAR"
