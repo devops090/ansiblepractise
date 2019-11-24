5c80c51043d4c0f25cd1c181f57e7c79905af5ac
devops090 <51469018+devops090@users.noreply.github.com>
2019-11-24 15:08:59 +0530

    Update runtime.sh
    fsfd

--- runtime.sh
+++ runtime.sh
@@ -9,4 +9,4 @@ echo $FINALVER
 echo $http_url
 echo $URL
 #ansible-playbook playbooks/studentapp-deploy.yaml -i inventory -u student --extra-vars "ansible_sudo_pass=student"
-ansible-playbook playbooks/studentapp-deploy.yaml -e  {"STUDENTWAR": "URL"} -i inventory --user=student --extra-vars "ansible_sudo_pass=student"
+ansible-playbook playbooks/studentapp-deploy.yaml -e '{"STUDENTWAR": "URL"}' -i inventory --user=student --extra-vars "ansible_sudo_pass=student"
