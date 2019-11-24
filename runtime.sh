ce8c7bf1a249b8511c17e23ed15f1f1703b0189f
devops090 <51469018+devops090@users.noreply.github.com>
2019-11-24 12:20:56 +0530

    Update runtime.sh
    bhjbjh

--- runtime.sh
+++ runtime.sh
@@ -2,7 +2,7 @@ VER=$(curl http://admin:admin123@10.128.0.7:8081/repository/studenapp-snapshot/c
 IDNO=$(echo $VER | awk -F - '{print $1F}')
 FINALVER=$(curl http://admin:admin123@10.128.0.7:8081/service/rest/repository/browse/studenapp-snapshot/com/jdevs/studentapp/$VER/ | html2text | grep "$IDNO-" | tail -1 | awk -F  - '{print $1F}')
 http_url=http://admin:admin123@10.128.0.7:8081/repository/studenapp-snapshot/com/jdevs/studentapp
-STUDENTWAR=$http_url/$VER/studentapp-$FINALVERabc
+STUDENTWAR=$http_url/$VER/studentapp-$FINALVER
 echo $VER
 echo $IDNO
 echo $FINALVER
