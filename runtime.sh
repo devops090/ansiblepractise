93a199a00db55527b1b0d60c6549ab830e9f3c2c
devops090 <51469018+devops090@users.noreply.github.com>
2019-11-24 11:56:57 +0530

    Update runtime.sh
    ksjbfjskd

--- runtime.sh
+++ runtime.sh
@@ -1,6 +1,6 @@
 VER=$(curl http://admin:admin123@10.128.0.7:8081/repository/studenapp-snapshot/com/jdevs/studentapp/maven-metadata.xml | grep SNAPSHOT | sed -e 's/<version>//g' -e 's/<\/version>//g' | xargs | xargs -n1 | sort | tail -1)
 IDNO=$(echo $VER | awk -F - '{print $1F}')
-FINALVER=$(curl http://admin:admin123@10.128.0.7:8081/service/rest/repository/browse/studenapp-snapshot/com/jdevs/studentapp/$VER/ | html2text | grep "$IDNO-" | tail -1 | xargs | xargs -n2)
+FINALVER=$(curl http://admin:admin123@10.128.0.7:8081/service/rest/repository/browse/studenapp-snapshot/com/jdevs/studentapp/$VER/ | html2text | grep "$IDNO-" | tail -1)
 http_url=http://admin:admin123@10.128.0.7:8081/repository/studenapp-snapshot/com/jdevs/studentapp
 STUDENTWAR=$http_url/$VER/studentapp-$FINALVER.war
 echo $VER
