55d9f3283f269d00db5d9ec6cef5f23574eb43be
devops090 <51469018+devops090@users.noreply.github.com>
2020-01-11 16:01:23 +0530

    Update runtime.sh
    google

--- runtime.sh
+++ runtime.sh
@@ -1,7 +1,7 @@
-VER=$(curl http://admin:admin123@10.128.0.7:8081/repository/studenapp-snapshot/com/jdevs/studentapp/maven-metadata.xml | grep SNAPSHOT | sed -e 's/<version>//g' -e 's/<\/version>//g' | xargs | xargs -n1 | sort | tail -1)
+VER=$(curl http://admin:admin123@10.128.0.5:8081/repository/studenapp-snapshot/com/jdevs/studentapp/maven-metadata.xml | grep SNAPSHOT | sed -e 's/<version>//g' -e 's/<\/version>//g' | xargs | xargs -n1 | sort | tail -1)
 IDNO=$(echo $VER | awk -F - '{print $1F}')
-FINALVER=$(curl http://admin:admin123@10.128.0.7:8081/service/rest/repository/browse/studenapp-snapshot/com/jdevs/studentapp/$VER/ | html2text | grep "$IDNO-" | tail -1 | awk -F  - '{print $nF}' | cut -d " " -f1)
-http_url=http://admin:admin123@10.128.0.7:8081/repository/studenapp-snapshot/com/jdevs/studentapp
+FINALVER=$(curl http://admin:admin123@10.128.0.5:8081/service/rest/repository/browse/studenapp-snapshot/com/jdevs/studentapp/$VER/ | html2text | grep "$IDNO-" | tail -1 | awk -F  - '{print $nF}' | cut -d " " -f1)
+http_url=http://admin:admin123@10.128.0.5:8081/repository/studenapp-snapshot/com/jdevs/studentapp
 URL=$http_url/$VER/studentapp-$FINALVER.war
 echo $VER
 echo $IDNO
@@ -11,4 +11,4 @@ echo $URL
 STUDENTWAR=$URL
 echo $STUDENTWAR
 #ansible-playbook playbooks/studentapp-deploy.yaml -i inventory -u student --extra-vars "ansible_sudo_pass=student"
-ansible-playbook playbooks/studentapp-deploy.yaml -i inventory --user=student --extra-vars "ansible_sudo_pass=student" -e "STUDENTWAR=$STUDENTWAR"
+ansible-playbook playbooks/studentapp-deploy.yaml -i inventory --user=stage --extra-vars "ansible_sudo_pass=stage" -e "STUDENTWAR=$STUDENTWAR"
